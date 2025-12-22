4c97eb04fefbf96353383e6970f5c0738833073b
qyf0310912 <qyf0310-_-@nate.com>
2025-12-23 02:04:03 +0900

    Create items.js

---
+++ items.js
@@ -0,0 +1,33 @@
+// 엄가네 품목 데이터 (대량 업로드용)
+// 카테고리: 과일 / 야채 / 잎채소 / 생선 / 기타
+
+window.ITEMS = [
+  // 과일
+  { cat: "과일", name: "사과", unit: "개", price: 3000 },
+  { cat: "과일", name: "배", unit: "개", price: 3500 },
+  { cat: "과일", name: "바나나", unit: "송이", price: 4000 },
+  { cat: "과일", name: "귤", unit: "봉", price: 6000 },
+  { cat: "과일", name: "딸기", unit: "팩", price: 9000 },
+
+  // 야채
+  { cat: "야채", name: "양파", unit: "망", price: 8000 },
+  { cat: "야채", name: "감자", unit: "봉", price: 7000 },
+  { cat: "야채", name: "당근", unit: "봉", price: 6000 },
+  { cat: "야채", name: "대파", unit: "단", price: 3000 },
+  { cat: "야채", name: "마늘", unit: "망", price: 10000 },
+
+  // 잎채소
+  { cat: "잎채소", name: "깻잎", unit: "봉", price: 6000 },
+  { cat: "잎채소", name: "상추", unit: "봉", price: 5000 },
+  { cat: "잎채소", name: "시금치", unit: "봉", price: 4000 },
+  { cat: "잎채소", name: "쑥갓", unit: "봉", price: 4000 },
+
+  // 생선
+  { cat: "생선", name: "고등어", unit: "마리", price: 5000 },
+  { cat: "생선", name: "갈치", unit: "마리", price: 9000 },
+
+  // 기타
+  { cat: "기타", name: "계란", unit: "판", price: 7000 },
+  { cat: "기타", name: "두부", unit: "모", price: 2000 },
+  { cat: "기타", name: "콩나물", unit: "봉", price: 2000 }
+];
